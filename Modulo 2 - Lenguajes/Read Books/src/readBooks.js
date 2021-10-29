acabe10e878d7e700b88289b4b6aa16d46191ff4
Estela <estelajimero@users.noreply.github.com>
2021-10-29 12:51:23 +0200

    Add isBookRead function

---
+++ Modulo 2 - Lenguajes/Read Books/src/readBooks.js
@@ -0,0 +1,17 @@
+const isBookRead = (books, titleToSearch) => {
+    let findBook = books.find(book => book.title === titleToSearch);
+
+    return Boolean(findBook) && findBook.isRead;
+}
+
+let books = [
+    { title: "Harry Potter y la piedra filosofal", isRead: true },
+    { title: "Canción de hielo y fuego", isRead: false },
+    { title: "Devastación", isRead: true },
+  ];
+
+console.log(isBookRead(books, "Devastación"));
+console.log(isBookRead(books, "Canción de hielo y fuego"));
+console.log(isBookRead(books, "Los Pilares de la Tierra"));
+
+// Opcional
