62c31dd7f352f53299f4159634659b9224579599
Estela <estelajimero@users.noreply.github.com>
2021-11-02 13:05:40 +0100

    Include types

---
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/01 deliverables/exercise-01.ts
@@ -0,0 +1,33 @@
+// @ts-check
+
+console.log("************** ARRAY METHODS *********************");
+// Head
+const newArr: number[] = [1, 2, 3, 4];
+const wordArr: string[] = ["queso", "chocolate", "tomate", "sal"];
+
+const head = ([first]) => first;
+
+console.log("--- HEAD ---");
+console.log(head(newArr));
+console.log(head(wordArr));
+
+// Tail
+const tail = ([_first, ...rest]) => rest;
+
+console.log("--- TAIL ---");
+console.log(tail(newArr));
+console.log(tail(wordArr));
+
+// Init
+const init = arr => arr.slice(0, -1);
+
+console.log("--- INIT ---");
+console.log(init(newArr));
+console.log(init(wordArr));
+
+// Last
+const last = arr => arr.slice(-1); 
+
+console.log("--- LAST ---");
+console.log(last(newArr));
+console.log(last(wordArr));
---
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/01 deliverables/exercise-02.ts
@@ -0,0 +1,22 @@
+console.log("************** CONCAT *********************");
+const numArr: number[] = [1, 2, 3, 4];
+const wordsArr: string[] = ["queso", "chocolate", "tomate", "sal"];
+const mixArr: [string, number, string, string, number] = ["Sevilla", 34, "Berlín", "45673982-D", 22];
+const objectArr: object[] = [
+    {
+        name: "Leo",
+        age: 34,
+        city: "Lugo",
+    }
+];
+
+const concat = (arr1, arr2) => console.log([...arr1, ...arr2]);
+
+concat(numArr, wordsArr);
+concat(wordsArr, numArr);
+
+// Opcional
+const concatMulti = (...arr) => arr;
+
+console.log(concatMulti(numArr, wordsArr, mixArr));
+console.log(concatMulti(numArr, wordsArr, mixArr, objectArr));
