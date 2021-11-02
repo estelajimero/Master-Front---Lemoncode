a7d39b97f6db84414e5e1d517688b6d0beecbbc9
Estela <estelajimero@users.noreply.github.com>
2021-11-02 14:07:09 +0100

    Add randomBool method

---
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/01 deliverables/exercise-05.ts
@@ -0,0 +1,42 @@
+console.log("************** SLOT MACHINE *********************");
+class SlotMachine {
+    count: number;
+
+    constructor () {
+        this.count = 0;
+    }
+
+    randomBool() {
+        return Math.random() > 0.5;
+    }
+
+    play() {
+        this.count++;
+
+        let arrBool = [
+            this.randomBool(),
+            this.randomBool(),
+            this.randomBool(),
+        ]
+
+        let winner: string = `Congratulations!!! You won ${this.count} coins!!`;
+        let loser: string = `Good luck next time!!`;
+
+        if (arrBool.some(element => element === false)) {
+            console.log(loser);
+        } else {
+            console.log(winner);
+            this.count = 0;
+        }
+    }
+}
+
+const machine1 = new SlotMachine();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
