171aa7da0dd1d50f4e87f0bc7c0480d46a2ec00f
Estela <estelajimero@users.noreply.github.com>
2021-11-08 11:51:06 +0100

    Add challenges ts files

--- Modulo 2 - Lenguajes/03-playground-exercises/src/index.ts
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/index.ts
@@ -6,6 +6,10 @@ import "./00 practice/morse.js";
 // import "./01 deliverables/exercise-03.js";
 // import "./01 deliverables/exercise-04.js";
 // import "./01 deliverables/exercise-05.js";
-// import "./02 challenges/exercise.js";
+// import "./02 challenges/consoleTraces.js";
+// import "./02 challenges/deepAccess.js";
+// import "./02 challenges/flattenArray.js";
+// import "./02 challenges/memoization.js";
+// import "./02 challenges/tree.js";
 
 console.log("Welcome to Exercises Playground.");
