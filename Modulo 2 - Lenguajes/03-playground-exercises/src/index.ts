b49eaa7875d12f0ef2dcfe350cb73e43ba4b5937
Estela <estelajimero@users.noreply.github.com>
2021-11-08 12:24:15 +0100

    Delete console.log

--- Modulo 2 - Lenguajes/03-playground-exercises/src/index.ts
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/index.ts
@@ -1,15 +1,14 @@
 // import "./00 practice/calificationsSummary.js";
-import "./00 practice/currySetter.js";
-import "./00 practice/morse.js";
+// import "./00 practice/currySetter.js";
+// import "./00 practice/morse.js";
 // import "./01 deliverables/exercise-01.js";
 // import "./01 deliverables/exercise-02.js";
 // import "./01 deliverables/exercise-03.js";
 // import "./01 deliverables/exercise-04.js";
 // import "./01 deliverables/exercise-05.js";
-// import "./02 challenges/consoleTraces.js";
+import "./02 challenges/consoleTraces.js";
 // import "./02 challenges/deepAccess.js";
 // import "./02 challenges/flattenArray.js";
 // import "./02 challenges/memoization.js";
 // import "./02 challenges/tree.js";
 
-console.log("Welcome to Exercises Playground.");
