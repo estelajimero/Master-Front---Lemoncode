9f918e5500a0a0b47f5cd6aa1ee8ad590195692a
Estela <estelajimero@users.noreply.github.com>
2021-11-09 12:29:15 +0100

    Create deepGet

--- Modulo 2 - Lenguajes/03-playground-exercises/src/02 challenges/deepAccess.ts
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/02 challenges/deepAccess.ts
@@ -7,27 +7,37 @@ console.log("********** DEEP ACCESS **********");
 // Implementa un mecanismo `deepGet` para acceder en profundidad a objetos anidados, de modo que podamos recuperar una propiedad en cualquiera de sus niveles. 
 // Mira a continuación el comportamiento que debería seguir:
 
-// const myObject = {
-//   a: 1,
-//   b: {
-//     c: null,
-//     d: {
-//       e: 3,
-//       f: {
-//         g: "bingo",
-//       }
-//     }
-//   }
-// };
+const myObject: object = {
+  a: 1,
+  b: {
+    c: null,
+    d: {
+      e: 3,
+      f: {
+        g: "bingo",
+      }
+    }
+  }
+};
 
-// const deepGet = ¿..?
+const deepGet = (obj: object, ...props: string[]) : any => {
+    let result: any = obj;
+    
+    if (props.length === 0) return obj;
+    
+    result = props.reduce((element, key) => {
+        return (element && element[key] !== undefined) ? element[key] : undefined;
+    }, obj);
 
-// console.log(deepGet(myObject, "x")); // undefined
-// console.log(deepGet(myObject, "a")); // 1
-// console.log(deepGet(myObject, "b")); // { c: null, d: {....}}
-// console.log(deepGet(myObject, "b", "c")); // null
-// console.log(deepGet(myObject, "b", "d", "f", "g")); // bingo
-// console.log(deepGet(myObject));  // {a: 1, b: {...}}
+    return result;
+}
+
+console.log(deepGet(myObject, "x")); // undefined
+console.log(deepGet(myObject, "a")); // 1
+console.log(deepGet(myObject, "b")); // { c: null, d: {....}}
+console.log(deepGet(myObject, "b", "c")); // null
+console.log(deepGet(myObject, "b", "d", "f", "g")); // bingo
+console.log(deepGet(myObject));  // {a: 1, b: {...}}
 // ```
 
 // ## Apartado B
