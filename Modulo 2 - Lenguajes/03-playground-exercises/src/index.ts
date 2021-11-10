65fe224d51bc2ac3e143a46ee28f6c3f49838295
Estela <estelajimero@users.noreply.github.com>
2021-11-10 18:15:02 +0100

    Change path

--- Modulo 2 - Lenguajes/03-playground-exercises/src/index.ts
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/index.ts
@@ -4,8 +4,8 @@
 // import "./01 deliverables/exercise-04.js";
 // import "./01 deliverables/exercise-05.js";
 // import "./02 challenges/consoleTraces.js";
-import "./02 challenges/deepAccess.js";
-// import "./02 challenges/flattenArray.js";
+// import "./02 challenges/deepAccess.js";
+import "./02 challenges/flattenArray.js";
 // import "./02 challenges/memoization.js";
 // import "./02 challenges/tree.js";
 // import "./03 extra/calificationsSummary.js";
