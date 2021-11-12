8aa0d620f5d1070bf489312f1cf55b31b3d6885d
Estela <estelajimero@users.noreply.github.com>
2021-11-12 10:37:17 +0100

    Add deepSet

--- Modulo 2 - Lenguajes/03-playground-exercises/src/02 challenges/deepAccess.ts
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/02 challenges/deepAccess.ts
@@ -43,13 +43,52 @@ console.log(deepGet(myObject));  // {a: 1, b: {...}}
 
 // Ahora implementa el complementario, `deepSet`, que permita guardar valores en profundidad. Su comportamiento debería ser:
 
-// console.log("---- Deep Set ----");
+/* por cada propiedad
+si es la ultima propiedad seteo el valor
+si no es la última tengo que seguir adentrándome. Si el valor actual de la propiedad no es objeto, crearlo
+establece el foco en el último objeto */
 
-// deepSet(1, myObject2, "a", "b");
-// console.log(JSON.stringify(myObject2));  // {a: { b: 1}}
-// deepSet(2, myObject2, "a", "c");
-// console.log(JSON.stringify(myObject2));  // {a: { b: 1, c: 2}}
-// deepSet(3, myObject2, "a");
-// console.log(JSON.stringify(myObject2));  // {a: 3}
-// deepSet(4, myObject2);
-// console.log(JSON.stringify(myObject2));  // Do nothing // {a: 3}
+console.log("---- Deep Set ----");
+
+const myObject2: object = {};
+
+const deepSet = (value: number, obj: object, ...setters: string[]) => {
+  if (!setters.length) return;
+
+  const [first, ...rest] = setters;
+
+  if (!rest.length) {
+    obj[first] = value;
+    return;
+  }
+
+  if (typeof obj[first] !== "object") {
+    obj[first] = {};
+  }
+
+  return deepSet(value, obj[first], ...rest);
+};
+
+// const deepSet = (value, obj, ...props) => {
+//   let target = obj;
+  
+//   for (let i = 0; i < props.length; i++) {
+//     const prop = props[i];
+
+//     if (i === props.length - 1) {
+//       target[prop] = value;
+//     } else {
+//       if (typeof target[prop] !== 'object') target[prop] = {}
+//       target = target[prop]
+//     }
+//   }
+// };
+
+deepSet(1, myObject2, "a", "b");
+console.log(JSON.stringify(myObject2));  // {a: { b: 1}}
+deepSet(2, myObject2, "a", "c");
+console.log(JSON.stringify(myObject2));  // {a: { b: 1, c: 2}}
+deepSet(3, myObject2, "a");
+console.log(JSON.stringify(myObject2));  // {a: 3}
+deepSet(4, myObject2);
+console.log(JSON.stringify(myObject2));  // Do nothing // {a: 3}
