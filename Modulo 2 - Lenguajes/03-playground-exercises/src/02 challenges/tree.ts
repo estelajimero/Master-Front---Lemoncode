7779d1eb982eae89fdcfb7d9c3dd2841c222ce53
Estela <estelajimero@users.noreply.github.com>
2021-11-12 10:13:54 +0100

    Add console.log

--- Modulo 2 - Lenguajes/03-playground-exercises/src/02 challenges/tree.ts
+++ Modulo 2 - Lenguajes/03-playground-exercises/src/02 challenges/tree.ts
@@ -6,3 +6,53 @@ console.log("********** TREE **********");
 
 // Un árbol es una estructura que parte de un nodo raiz, a partir del cual salen más nodos. 
 // Cada nodo en un árbol puede tener hijos (más nodos) o no tenerlos (convirtiendose en un nodo final o una "hoja").
+
+interface ProductTreeNode <T> {
+    id?: number,
+    name: string,
+    product?: ProductTreeNode <T> [],
+}
+
+const productTree: ProductTreeNode<string> = {
+    id: 765,
+    name: "Lácteos",
+    product: [
+        {
+            id: 765-21,
+            name: "Queso fresco",
+        },
+        {
+            id: 765-22,
+            name: "Yogures",
+            product: [
+                {
+                    id: 765-220,
+                    name: "Yogur griego natural",
+                },
+                {
+                    id: 765-221,
+                    name: "Yogures de soja (pack)",
+                },
+            ]
+        },
+        {
+            id: 765-23,
+            name: "Leche",
+            product: [
+                {
+                    id: 765-230,
+                    name: "Leche entera",
+                },
+                {
+                    id: 765-231,
+                    name: "Leche de avena",
+                }
+            ],
+        }
+    ],
+}  
+
+
+console.log(JSON.stringify(productTree));
+console.log(productTree);
+
