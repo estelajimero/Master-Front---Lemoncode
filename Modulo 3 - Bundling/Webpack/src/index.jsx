878314cc6a06cc43b24b0dd37c28430d63e7108a
Estela <estelajimero@users.noreply.github.com>
2021-11-16 11:46:41 +0100

    Import components

---
+++ Modulo 3 - Bundling/Webpack/src/index.jsx
@@ -0,0 +1,28 @@
+import "./mystyles.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import landscapeImg from "./content/iceland-1.jpg";
+
+import { HelloWorldComponent } from "./components/helloWorld/HelloWorldComponent";
+import ContactList from "./components/list/ListComponent";
+
+const img = document.createElement("img");
+img.src = landscapeImg;
+
+document.getElementById("imgContainer").appendChild(img);
+
+const contacts = [
+    { id: 1, name: "Leanne Graham" },
+    { id: 2, name: "Ervin Howell" },
+    { id: 3, name: "Clementine Bauch" },
+    { id: 4, name: "Patricia Lebsack" }
+  ];
+
+ReactDOM.render(
+    <div>
+        <HelloWorldComponent />
+        <ContactList contacts={contacts} />
+    </div>,
+    document.getElementById('root')
+  );
