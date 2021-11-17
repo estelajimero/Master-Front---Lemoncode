bf447458b0574aed469a46e1295eb6e94fd67c76
Estela <estelajimero@users.noreply.github.com>
2021-11-17 12:16:02 +0100

    Create react components

---
+++ Modulo 3 - Bundling/Parcel - v1/src/components/helloWorld/HelloWorldComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./helloWorldComponentStyles.scss";
+// import { showEnv } from "../../variables";
+
+export const HelloWorldComponent = () => {
+  return (
+    <>
+      <h1 className={classes.title}>Hello World!</h1>
+    </>
+  );
+};
+
+// showEnv();
---
+++ Modulo 3 - Bundling/Parcel - v1/src/index.tsx
@@ -0,0 +1,36 @@
+import "./styles.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import landscapeImg from "./content/iceland-1.jpg";
+
+import { HelloWorldComponent } from "./components/helloWorld/HelloWorldComponent";
+import ContactList from "./components/list/ListComponent";
+
+const img = document.createElement("img");
+img.src = landscapeImg;
+
+document.getElementById("imgContainer").appendChild(img);
+
+interface Contact {
+  id: number;
+  name: string;
+}
+
+const contacts: Contact[] = [
+  { id: 1, name: "Leanne Graham" },
+  { id: 2, name: "Ervin Howell" },
+  { id: 3, name: "Clementine Bauch" },
+  { id: 4, name: "Patricia Lebsack" },
+];
+
+ReactDOM.render(
+  <div>
+    <HelloWorldComponent />
+    <ContactList contacts={contacts} />
+  </div>,
+  document.getElementById("root")
+);
+
+
+
+
