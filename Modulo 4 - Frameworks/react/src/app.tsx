10c6c0fa1887c086949298990180dd972405de2e
Estela <estelajimero@users.noreply.github.com>
2022-01-09 18:35:47 +0100

    First commit

---
+++ Modulo 4 - Frameworks/react/src/app.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { MembersTable } from "./components/members-table";
+import { DetailPage } from "./detail";
+import { LoginPage } from "./login";
+
+export const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/list" element={<MembersTable />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+      </Routes>
+    </Router>
+  );
+};
