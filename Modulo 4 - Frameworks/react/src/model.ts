74152fcff799a9828a6581bef847fe4b6b7b7af2
Estela <estelajimero@users.noreply.github.com>
2022-01-09 18:36:19 +0100

    Create model

---
+++ Modulo 4 - Frameworks/react/src/model.ts
@@ -0,0 +1,14 @@
+export interface MemberEntity {
+  avatar_url: string;
+  id: string;
+  login: string;
+}
+
+export interface MemberDetailEntity {
+  id: string;
+  login: string;
+  name: string;
+  company: string;
+  bio: string;
+}
+
