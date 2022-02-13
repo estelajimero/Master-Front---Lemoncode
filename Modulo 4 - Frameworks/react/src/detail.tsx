e5715c860dbec2190de1cc8bf4258701b226a986
Estela <estelajimero@users.noreply.github.com>
2022-02-13 11:18:32 +0100

    Add detail page

--- Modulo 4 - Frameworks/react/src/detail.tsx
+++ Modulo 4 - Frameworks/react/src/detail.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { MemberDetailEntity } from "./model";
 
+import * as classes from "./detail.styles";
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+
 const createDefaultMemberDetail = () => ({
 	id: "",
 	login: "",
@@ -17,15 +24,35 @@ export const DetailPage: React.FC = () => {
 
 	const { id } = useParams();
 
+	React.useEffect(() => {
+		fetch(`https://api.github.com/users/${id}`)
+			.then((response) => response.json())
+			.then((json) => setMember(json));
+	}, []);
+
 	return (
 		<>
-			<h2>Hello from Detail page</h2>
-			<p>Id: {member.id}</p>
-			<p>Login: {member.login}</p>
-			<p>Name: {member.name}</p>
-			<p>Company: {member.company}</p>
-			<p>Bio: {member.bio}</p>
-			<Link to="/list">Back to list page</Link>
+			<Typography className={classes.detailTitle} variant="h5" component="h2">{member.name}</Typography>
+			<Card className={classes.root}>
+				<CardContent className={classes.cardContent}>
+					<Typography className={classes.memberName} variant="h5" component="h2">
+						Name: {member.name}
+					</Typography>
+					<Typography className={classes.memberId} color="textSecondary" gutterBottom>
+						ID: {id}
+					</Typography>
+					<Typography className={classes.memberLogin} color="textSecondary">
+						Login: {member.login}
+					</Typography>
+					<Typography className={classes.memberCompany} color="textSecondary">
+						Company: {member.company}
+					</Typography>
+					<Typography variant="body2" component="p">
+						Bio: {member.bio}
+					</Typography>
+				</CardContent>
+				<Link className={classes.linkStyle} to="/list">Back to list page</Link>
+			</Card>
 		</>
 	);
 };
